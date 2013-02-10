7fae256117e09ed197bd175fa81f2e7c6536bcc0
garsh0p <garsh0p@gmail.com>
2013-02-09 19:57:26 -0600

    asdfsadf

--- web/app/Controller/MainController.php
+++ web/app/Controller/MainController.php
@@ -44,7 +44,7 @@ class MainController extends AppController {
  *
  * @var array
  */
-	public $uses = array();
+	public $uses = array('User');
 
 /**
  * Displays a view
@@ -63,8 +63,13 @@ class MainController extends AppController {
 	}
 
 	public function test() {
-		QRcode::png('code data text', 'filename.png'); // creates file 
-		$this->set('test', 'About');
+        $this->User->set('id', '2');
+        $test = $this->User->find('all');
+        $this->User->save();
+
+        $testUser = $this->User->find('all', array('conditions'=>array('User.id'=>1)));
+        $this->set('test', $testUser);
+
 		$this->render('/Pages/test');
 	}
 }
--- web/app/View/Pages/test.ctp
+++ web/app/View/Pages/test.ctp
@@ -1,21 +1,3 @@
-<?php $this->assign('title', "QRapp.ly - About"); ?>
-
-<?php  $this->start('css'); ?>
-  <!-- Supply additional CSS here -->
-<?php $this->end('css'); ?>
-
-<div id="qrcode"></div>
-
-<?php $this->start('script'); ?>
-
-<script type="text/javascript" src="/js/lib/jquery.qrcode.js"></script>
-<script type="text/javascript" src="/js/lib/qrcode.js"></script>
-
-<script type="text/javascript">
-	jQuery('#qrcode').qrcode({
-		text	: "http://jetienne.com"
-	});	
-</script>
-
-
-<?php $this->end('script'); ?>
+<?php
+    print_r($test);
+?>
